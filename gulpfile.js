79b2f08893757807aae7a742ad4dcc548f199783
Gener Zuniga <gener.zuniga@havasmagma.com>
2016-10-03 22:36:11 -0600

    updating gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,15 @@
 var gulp = require("gulp"),
-	gutil = require("gulp-util");
+	gutil = require("gulp-util"),
+	coffee = require("gulp-coffee");
 
 gulp.task('log', function() {
-	gutil.log(workflot-ing);
-})
+	gutil.log("workflot-ing");
+});
+
+var coffeeSources = ['components/coffee/tagline.coffee']
+gulp.task('coffee', function () {
+	gulp.src(coffeeSources) //'components/coffee/*.coffee'
+		.pipe(coffee({ bare: true }) //compile js out of safety wrapper
+			.on('error', gutil.log))
+		.pipe(gulp.dest('components/scripts'))
+});
